8f2bd8fc2b4a857ecd6dbdf8be8b377374f6f4c2
Chandan Vishwakarma <your-github-vchandan402@gmail.com>
2026-06-15 14:48:57 +0530

    new update

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -14,7 +14,7 @@ class RegistrationController extends Controller
 
     public function register(Request $request)
     {
-        return "Registration Success";
+        return "Registration Success chandan";
     }
 }
 use App\Http\Controllers\HelloController;
